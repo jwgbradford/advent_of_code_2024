a2b43a5ab022f3a4454fdc90c7e5a58097e8aa0d
John Bradford <jwgbradford@gmail.com>
2024-12-02 13:06:56 +0000

    2_2 test working

--- day2_2.py
+++ day2_2.py
@@ -1,13 +1,17 @@
-''' count where both of the following are true:
+''' count where all of the following are true:
 1) The levels are either all increasing or all decreasing.
 2) Any two adjacent levels differ by at least one and at most three.
+3) If removing a single level from an unsafe report would make it safe, the report instead counts as safe
 '''
+
 from aoc_common import get_data
 
 def count_safe(data : list[list[int]]) -> int:
     safe_count : int = 0
     for report in data:
         if check_safe(report):
+            safe_count += 1
+        elif problem_dampener(report):
             safe_count += 1
     return safe_count
 
@@ -31,5 +35,16 @@ def check_safe(data : list[int]) -> bool:
             return False
     return True
 
-my_data = get_data('input2.txt')
+def problem_dampener(data : list[int]) -> bool:
+    for index in range(len(data)):
+        # if you do test_level = data 
+        # it creates a pointer to data
+        # not a new list test_level
+        test_level = [l for l in data]
+        del test_level[index]
+        if check_safe(test_level):
+            return True
+    return False
+
+my_data = get_data('test2.txt')
 print(count_safe(my_data))
