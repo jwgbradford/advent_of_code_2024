f590a48b97b4a8d7ba9a17ef2c16ab161f3d4e7b
John Bradford <jwgbradford@gmail.com>
2024-12-03 13:36:40 +0000

    day 3 test working with regex

---
+++ day3_1.py
@@ -0,0 +1,18 @@
+'''
+What do you get if you add up all of the results of the multiplications?
+'''
+
+import re
+from aoc_common import read_file
+
+def calc_mul(data : str) -> int:
+    # you have a problem, you use regex
+    mul_list = re.findall("mul\(([0-9]+),([0-9]+)\)", data)
+    # now you have two problems
+    mul_calc = 0
+    for calc in mul_list:
+        mul_calc += int(calc[0]) * int(calc[1])
+    print(mul_calc)
+
+my_data = read_file('test3.txt')
+calc_mul(my_data)
